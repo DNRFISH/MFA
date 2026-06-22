8e585952878b54e5f8f994e4c0b79725de9e41eb
lrnathan <nathanl@michigan.gov>
2026-06-22 16:03:12 -0400

    created help file for merging branches into main

---
+++ dev/branch_merge_steps.R
@@ -0,0 +1,16 @@
+#help file for merging branches
+
+#need to follow this process in order to merge and then delete local branch
+
+#1: Commit and push changes from local machine
+#2: Go to Github (website)
+#3: Open Pull Request
+#4: Merge into main; use "merge and squash"
+#5: Delete branch (on GitHub); note that this resolves any linked issues!
+#6: Go back to R Studio to clean locally
+#7: Switch to main branch (git checkout main)
+#8: Pull updates (git pull)
+#9: Remove remote trackin references (git fetch -p)
+#10: Delete local branch (git branch -d [branch name]); if error, captial D forces delete; make sure main is updated!!
+#11: Refresh using git button or restart R studio
+
