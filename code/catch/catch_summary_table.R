2808f18f677477b4c99bcb9d6773ae7e95d6ceb6
lrnathan <nathanl@michigan.gov>
2026-06-30 11:17:36 -0400

    updated after adding catch data to FISH_query

--- code/catch/catch_summary_table.R
+++ code/catch/catch_summary_table.R
@@ -3,7 +3,7 @@
 #' Creates summary table of catches by species per survey
 
 #'
-#' @param catchData Data query from catchByEffort function
+#' @param catchData Data query from FISH_query function
 #' @return Summary Table
 #' @export
 #'
@@ -12,14 +12,19 @@
 
 catch_summary_table <- function(catchData) {
   
+  if(!"TotalNumberCaught" %in% names(catchData)){
+    stop("No catch data included. Need to provide catch data from FISH_query")
+  }
+  
   #do a check to see if multiple efforts are included in the table
-  if (length(unique(catchData$ModuleId)) > 1) {
+  if (length(unique(catchData$GearType)) > 1) {
     message(paste0("Warning: ",
-                   length(unique(catchData$ModuleId)),
-                   " ModuleIds included in catch summary data"))
+                   length(unique(catchData$GearType)),
+                   " different gears included in catch summary data"))
   }
   
   outTab<-catchData%>%
+    filter(!is.na(TotalNumberCaught))%>% #remove efforts with no catches
     group_by(SurveyId,Species)%>%
     summarise(
       TotalNumberCaught=sum(TotalNumberCaught,na.rm = T),
