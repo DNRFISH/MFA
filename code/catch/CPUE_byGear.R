d30f1a571c1e45094090a3b55cf6280e7221e6fe
lrnathan <nathanl@michigan.gov>
2026-07-01 11:24:04 -0400

    finished first draft of function added gear unit clean up

--- code/catch/CPUE_byGear.R
+++ code/catch/CPUE_byGear.R
@@ -7,10 +7,18 @@
 #' @return Summary Table or Figure
 #' @export
 #'
+#' @examples
+#' #Single Survey
+#' CatchEffortData <- FISH_query(con,QueryType = "Catch",SurveyId = 1162)
+#' CPUE<-CPUE_byGear(CatchEffortData)
+#' 
+#' #Multiple Surveys
+#' CatchEffortData <- FISH_query(con,QueryType = "Catch",SurveyPurpose = "Status & Trends",Year=2025)
+#' CPUE<-CPUE_byGear(CatchEffortData)
 #'
 
 
-CPUE_byGear<-function(CatchEffortData,OutputType=NULL){
+CPUE_byGear<-function(CatchEffortData,OutputType="Table"){
   
   #get efforts with NAs in effort quantity column for removal (e.g., limno or missing data)
   effortsNA<-CatchEffortData%>%
@@ -22,50 +30,79 @@ CPUE_byGear<-function(CatchEffortData,OutputType=NULL){
     message(paste0("WARNING:",nrow(effortsNA)," effort(s) removed from summary due to missing data. Look at raw output to determine missing data."))
   }
 
-  #summarize catch data
-  catchByGear<-CatchEffortData%>%
+  #clean up data
+  CatchEffortDataClean<-CatchEffortData%>%
+    #remove missing efforts (a warning will spit out above)
     filter(!is.na(EffortTotalQuantity) | !is.na(EffortAlternateQuantity))%>%
+    #clean up effort units and values
+    mutate(
+      #use the EffortTotalQuantity and EffortTotalMeasurement as primary, if exists; otherwise pull from alternate
+      use_total = !is.na(EffortTotalQuantity) & EffortTotalQuantity != 0,
+      Effort = if_else(
+        use_total,
+        EffortTotalQuantity,
+        EffortAlternateQuantity
+      ),
+      
+      Unit = case_when(
+        use_total ~ EffortTotalMeasurement,
+        TRUE      ~ EffortAlternateMeasurement
+      ),
+      
+      #use alterante unit and value, if not used for primary
+      Effort_alt = case_when(
+        use_total ~ EffortAlternateQuantity,
+        TRUE      ~ EffortTotalQuantity
+      ),
+      
+      Unit_alt = case_when(
+        use_total ~ EffortAlternateMeasurement,
+        TRUE      ~ EffortTotalMeasurement
+      )
+    )%>%
+    select(-c(EffortTotalQuantity,EffortTotalMeasurement,EffortAlternateQuantity,EffortAlternateMeasurement,use_total))
+    
+  #summarize catch data
+  catchByGear<-CatchEffortDataClean%>%
     filter(!is.na(Species))%>%
     group_by(SurveyId,GearType,Species)%>%
     summarise(TotalNumberCaught=sum(TotalNumberCaught,na.rm = T),.groups = "drop")
   
   #summarize effort data
-  effortByGear<-CatchEffortData%>%
-    filter(!is.na(EffortTotalQuantity) | !is.na(EffortAlternateQuantity))%>%
-    select(WaterBodyName,SurveyId,SurveyEffortId,SurveyEffortKey,GearType,EffortNumberofGearUsed,EffortTotalQuantity,EffortTotalMeasurement,EffortAlternateQuantity,EffortAlternateMeasurement)%>%
+  effortByGear<-CatchEffortDataClean%>%
+    select(WaterBodyName,SurveyId,SurveyEffortId,SurveyEffortKey,GearType,EffortNumberofGearUsed,Effort,Unit,Effort_alt,Unit_alt)%>%
     unique()%>%
-    group_by(WaterBodyName,SurveyId,GearType,EffortTotalMeasurement,EffortAlternateMeasurement)%>%
-    summarize(EffortTotalQuantity=sum(as.numeric(EffortTotalQuantity)),
-              EffortAlternateQuantity=sum(as.numeric(EffortAlternateQuantity)),
-              .groups = "drop")%>%
-    as.data.frame()
+    group_by(WaterBodyName,SurveyId,GearType,Unit,Unit_alt)%>%
+    summarize(TotalEffort=sum(as.numeric(Effort)),
+              TotalEffort_alt=sum(as.numeric(Effort_alt)),
+              .groups = "drop")
   
   
   #merge tables and calculate CPUE
   allDat<-effortByGear%>%
-    inner_join(catchByGear,by=c("SurveyId","GearType"))%>% #this join causing issues with many to many
+    inner_join(catchByGear,by=c("SurveyId","GearType"))%>%
     mutate(
-      EffortTotalQuantity = if_else(
-        EffortTotalMeasurement == "Seconds",
-        EffortTotalQuantity / 60,
-        EffortTotalQuantity
+      TotalEffort = if_else(
+        Unit == "Seconds",
+        TotalEffort / 60,
+        TotalEffort
       ),
-      EffortAlternateQuantity = if_else(
-        EffortAlternateMeasurement == "Seconds",
-        EffortAlternateQuantity / 60,
-        EffortAlternateQuantity
+      TotalEffort_alt = if_else(
+        Unit_alt == "Seconds",
+        TotalEffort_alt / 60,
+        TotalEffort_alt
       ),
-      EffortTotalMeasurement = if_else(
-        EffortTotalMeasurement == "Seconds", "Minutes", EffortTotalMeasurement
+      Unit = if_else(
+        Unit == "Seconds", "Minutes", Unit
       ),
-      EffortAlternateMeasurement = if_else(
-        EffortAlternateMeasurement == "Seconds", "Minutes", EffortAlternateMeasurement
+      Unit_alt = if_else(
+        Unit_alt == "Seconds", "Minutes", Unit_alt
       ),
-      CPUE=TotalNumberCaught/EffortTotalQuantity,
-      CPUE_alt=TotalNumberCaught/EffortAlternateQuantity)%>%
+      CPUE=TotalNumberCaught/TotalEffort,
+      CPUE_alt=TotalNumberCaught/TotalEffort_alt)%>%
     select(WaterBodyName,SurveyId,GearType,Species,TotalNumberCaught,
-           EffortTotalQuantity,EffortTotalMeasurement,CPUE,
-           EffortAlternateQuantity,EffortAlternateMeasurement,CPUE_alt)
+           TotalEffort,Unit,CPUE,
+           TotalEffort_alt,Unit_alt,CPUE_alt)
   
   if(OutputType=="Table"){
     return(allDat)
