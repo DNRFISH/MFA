84429948177699ffe960538bb8775b599c12be9c
lrnathan <nathanl@michigan.gov>
2026-08-21 14:33:44 -0400

    updated code to work with new data labels; had to fix "trophy" sum code

--- code/catch/RSD.R
+++ code/catch/RSD.R
@@ -2,10 +2,11 @@
 #'
 #' Creates summary table or graph of RSD values based on Gabelhouse length categories
 #'
-#' @param lengthFreqData Data query from lengthFreqbyEffort function
+#' @param lengthFreqData Data query from lengthFreqByEffort function
 #' @return Summary Table
 #' @export
 #'
+#' @details N.Stock means # of fish >=stock length but < Quality length. Total N.Stock = N.Stock+N.Quality+N.Preferred+N.Memorable+N.Trophy
 
 
 RSD<-function(lengthFreqData){
@@ -15,35 +16,34 @@ RSD<-function(lengthFreqData){
   }
   
   #read in gabelhouse lengths-- used relative path for app compatability
-  RSDvals<-readr::read_csv("data/Gabelhouse_RSD_lengths.csv")
+  RSDvals<-readr::read_csv("data/Gabelhouse_RSD_lengths.csv",show_col_types = FALSE)
   
   #add gabelhouse categories to length data
   labels<-c("Stock","Quality","Preferred","Memorable","Trophy")
-  lengthDatSub$RSDcat<-suppressWarnings(apply(lengthDatSub,1,function(x)
-    labels[max(which((as.numeric(x["Inch_Group"])>=
-                        as.numeric(RSDvals[match(x["Species_Code"],RSDvals$Species_Code),3:7]))==T))]))
+  lengthFreqData$RSDcat<-suppressWarnings(apply(lengthFreqData,1,function(x)
+    labels[max(which((as.numeric(x["InchGroup"])>=
+                        as.numeric(RSDvals[match(x["Species"],RSDvals$species),4:8]))==T))]))
   
   #RSD summary
-  RSDsum<-lengthDatSub%>%
-    filter(!is.na(RSDcat))%>%
-    group_by(Survey_Number,Species_Name)%>%
-    summarize(N.Stock=sum(Total_Number_Caught[RSDcat=="Stock"]),
-              N.Quality=sum(Total_Number_Caught[RSDcat=="Quality"]),
-              N.Preferred=sum(Total_Number_Caught[RSDcat=="Preferred"]),
-              N.Memorable=sum(Total_Number_Caught[RSDcat=="Memorable"]),
-              N.Trophy=sum(RSDcat=="Trophy"),
-              PSD=round(sum(Total_Number_Caught[RSDcat%in%c("Quality","Preferred","Memorable","Trophy")])/
-                          sum(Total_Number_Caught[RSDcat%in%c("Stock","Quality","Preferred","Memorable","Trophy")])*100),
-              RSD.p=round(sum(Total_Number_Caught[RSDcat%in%c("Preferred","Memorable","Trophy")])/
-                            sum(Total_Number_Caught[RSDcat%in%c("Stock","Quality","Preferred","Memorable","Trophy")])*100),
-              RSD.m=round(sum(Total_Number_Caught[RSDcat%in%c("Memorable","Trophy")])/
-                            sum(Total_Number_Caught[RSDcat%in%c("Stock","Quality","Preferred","Memorable","Trophy")])*100),
-              RSD.t=round(sum(Total_Number_Caught[RSDcat%in%c("Trophy")])/
-                            sum(Total_Number_Caught[RSDcat%in%c("Stock","Quality","Preferred","Memorable","Trophy")])*100))%>%
+  RSDsum<-lengthFreqData%>%
+    filter(Species%in%RSDvals$species)%>%
+    group_by(SurveyId,Species)%>%
+    summarize(N.Stock=sum(TotalNumberCaught[RSDcat=="Stock"],na.rm = T),
+              N.Quality=sum(TotalNumberCaught[RSDcat=="Quality"],na.rm = T),
+              N.Preferred=sum(TotalNumberCaught[RSDcat=="Preferred"],na.rm = T),
+              N.Memorable=sum(TotalNumberCaught[RSDcat=="Memorable"],na.rm = T),
+              N.Trophy=sum(TotalNumberCaught[RSDcat=="Trophy"],na.rm = T),
+              PSD=round(sum(TotalNumberCaught[RSDcat%in%c("Quality","Preferred","Memorable","Trophy")])/
+                          sum(TotalNumberCaught[RSDcat%in%c("Stock","Quality","Preferred","Memorable","Trophy")])*100),
+              RSD.p=round(sum(TotalNumberCaught[RSDcat%in%c("Preferred","Memorable","Trophy")])/
+                            sum(TotalNumberCaught[RSDcat%in%c("Stock","Quality","Preferred","Memorable","Trophy")])*100),
+              RSD.m=round(sum(TotalNumberCaught[RSDcat%in%c("Memorable","Trophy")])/
+                            sum(TotalNumberCaught[RSDcat%in%c("Stock","Quality","Preferred","Memorable","Trophy")])*100),
+              RSD.t=round(sum(TotalNumberCaught[RSDcat%in%c("Trophy")])/
+                            sum(TotalNumberCaught[RSDcat%in%c("Stock","Quality","Preferred","Memorable","Trophy")])*100),
+              .groups = "drop")%>%
     
     as.data.frame()
   
-  
-  message("Note that N.Stock means # of fish >=stock length but < Quality length. Total N.Stock = N.Stock+N.Quality+N.Preferred+N.Memorable+N.Trophy")
   return(RSDsum)
 }
