3523c29b17f8433a17c7fcdfa09b22dde8a4f306
lrnathan <nathanl@michigan.gov>
2026-09-15 10:47:42 -0400

    added tests; need to fix error with large queries #22 and add a test for efforts with zero catch #21

--- tests/testthat/test-FISH_query.R
+++ tests/testthat/test-FISH_query.R
@@ -2,42 +2,64 @@
 con<-FISHub_connect()
 
 
+#not currenlty working issue #22
+# test_that("FISH_query works with default QueryType", {
+#   result <- FISH_query(con)
+#   expect_true(is.data.frame(result))
+#   expect_gt(nrow(result), 0)
+# })
 
-test_that("FISH_query is working", {
-  surveyQuery<-FISH_query(con,QueryType = "Survey",SurveyPurpose = "Status & Trends",Year=2025)
-  expect_true(all(c(
-  "SurveyId",
-  "WaterBodyName",
-  "MDNRID"
-) %in% names(surveyQuery)))
-})
-
-test_that("FISH_query filters by SurveyId", {
-  result <- FISH_query(
-    con,
-    QueryType = "Catch",
-    SurveyId = 1162
-  )
+test_that("FISH_query is working, filters by SurveyID for each QueryType", {
+  survey <- FISH_query(con, QueryType = "Survey",SurveyId = 1162)
+  efforts <- FISH_query(con, QueryType = "Efforts",SurveyId = 1162)
+  catch <- FISH_query(con, QueryType = "Catch",SurveyId = 1162)
   
-  expect_true(all(result$SurveyId == 1162))
+  expect_true(all(c("SurveyId","WaterBodyName","MDNRID") %in% names(survey)))
+  expect_true(is.data.frame(survey))
+  expect_true(is.data.frame(efforts))
+  expect_true(is.data.frame(catch))
 })
 
-#need to work on this still... seems to be an issue with the multiple filters
-# test_that("FISH_query applies multiple filters", {
+
+test_that("FISH_query applies individual filters", {
+  result <- FISH_query(con,QueryType = "Survey",Year = 2025)
+  expect_gt(nrow(result), 0)
+  expect_true(all(result$Year == 2025))
+  
+  result <- FISH_query(con,QueryType = "Survey",SurveyPurpose = "Status & Trends",Year=2025)
+  expect_gt(nrow(result), 0)
+  expect_true(all(result$SurveyPurpose == "Status & Trends"))
+  
+  result <- FISH_query(con,QueryType = "Catch",MDNRID = "L7844")
+  expect_gt(nrow(result), 0)
+  expect_true(all(result$MDNRID == "L7844"))
+})
+
+
+test_that("FISH_query applies multiple filters", {
+  result <- FISH_query(con,QueryType = "Catch",GearType = "LMFYKE",Species = "Walleye")
+  expect_true(all(result$Species == "Walleye"))
+  expect_true(all(result$GearType == "LMFYKE"))
+  
+  result <- FISH_query(con,QueryType = "Catch",SurveyId = 1162,Species = "Largemouth Bass")
+  expect_true(all(result$SurveyId == 1162))
+  expect_true(all(result$Species == "Largemouth Bass"))
+})
+
+#**update this once I identify an effort** issue #21
+# test_that("FISH_query retains efforts with zero catch", {
 #   result <- FISH_query(
 #     con,
 #     QueryType = "Catch",
-#     GearType = "FykeNet",
-#     Species = "Walleye"
+#     SurveyId = SOME_KNOWN_SURVEY
 #   )
 #   
-#   expect_true(all(result$Year == 2025))
-#   expect_true(all(result$GearType == "FykeNet"))
+#   expect_true(SOME_KNOWN_EFFORT_ID %in% result$SurveyEffortId)
 # })
-# 
-# test_that("FISH_query rejects invalid QueryTypes", {
-#   expect_error(
-#     FISH_query(con, QueryType = "BadType"),
-#     "must be one of"
-#   )
-# })
+
+test_that("FISH_query rejects invalid queries", {
+  expect_error(FISH_query(con, QueryType = "BadType"))
+  expect_error(FISH_query(con, QueryType = "Survey",GearType="BadNet"))
+  expect_error(FISH_query(con, QueryType = "Survey",SurveyPurpose="BadPurpose"))
+})
+
